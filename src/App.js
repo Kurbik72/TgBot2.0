bd6a0927f0e10833fd708fb16b7e61c58095ed21
Kurbik72 <jumperchannel79@gmail.com>
2024-10-14 15:15:29 +0300

    add new route system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 import { Vizit } from './components/vizitPage/vizitpage';
 
 import Component from './components/pup/Component';
@@ -13,8 +13,9 @@ function App  () {
     <div className="App">
 <BrowserRouter>
       <Routes>
-      <Route path="/welcomepage" element={<Component />}/>
+      <Route path="welcomepage" element={<Component />}/>
         <Route path="visitpage" element={<Vizit/>}/>
+        <Route path="/" element={<Navigate to="/welcomepage" replace />} />
         </Routes>
       </BrowserRouter>
       
